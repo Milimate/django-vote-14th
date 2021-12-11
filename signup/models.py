fbc773abe7c476846b851c00c9cc9769f711822a
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-12-11 19:54:22 +0900

    feat(models.py): add entities and UserManager to use AbstractBaseUser

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, password, email):
+
+        if not username:
+            raise ValueError('must have username')
+        if not email:
+            raise ValueError('must have user email')
+        if not password:
+            raise ValueError('must have user password')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, email):
+
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=255,
+        unique=True,
+    )
+
+    username = models.CharField(
+        verbose_name='Username',
+        max_length=50,
+        unique=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
