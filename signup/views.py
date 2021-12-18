5a070c70e698e56b604ba585bdc1c0d0a33cdb7c
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-12-18 17:03:38 +0900

    feat(views.py): add POST api

--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from signup.models import User
+from signup.serializers import SignupSerializer
+
+
+@csrf_exempt
+def signup_list(request):
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SignupSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
