329f70e1f4789bd95ddfce29769cbc2400287627
Sukyeong Seo <tjtnrud0967@gmail.com>
2021-12-23 11:28:40 +0900

    feat(serializers.py): add signup serializer

--- signup/serializers.py
+++ signup/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from signup.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['id', 'username', 'password', 'email']
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def create(self, validated_data):
+            vote_user = User.objects.create_user(
+                validated_data["username"], validated_data["email"], validated_data["password"]
+            )
+
+            return vote_user
