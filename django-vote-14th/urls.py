92e5df1205b1b7558cf437d9ab051b1b3470456f
Sukyeong Seo <tjtnrud0967@gmail.com>
2021-12-23 11:03:19 +0900

    feat(urls.py): add signup url

--- django-vote-14th/urls.py
+++ django-vote-14th/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('signup.urls'))
+    path('ceos-15th-partzzang/', include('signup.urls'))
 ]
