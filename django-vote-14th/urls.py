5337493e565c2d98a9e870944923c135cfcc1b4a
Sukyeong Seo <tjtnrud0967@gmail.com>
2021-12-24 01:33:09 +0900

    docs(urls.py)

--- django-vote-14th/urls.py
+++ django-vote-14th/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ceos-15th-partzzang/', include('signup.urls'))
+    path('ceos-15th-partzzang/', include('signup.urls')),
     path('ceos-15th-partzzang/', include('login.urls')),
     path('ceos-15th-partzzang/', include('vote.urls')),
 ]
