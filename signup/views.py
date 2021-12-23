d3d47e0cd490c04b5c72ade45e05ba8836c464da
Sukyeong Seo <tjtnrud0967@gmail.com>
2021-12-23 11:28:00 +0900

    feat(views.py): add signup view

--- signup/views.py
+++ signup/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from signup.models import User
-from signup.serializers import SignupSerializer
+from .models import User
+from .serializers import SignupSerializer, UserSerializer
 
 
-@csrf_exempt
-def signup_list(request):
+class SignupView(ListCreateAPIView):
+    create_queryset = User.objects.all()
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
 
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SignupSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        newUser = serializer.save()
 
+        return Response(
+            {
+                "user": UserSerializer(newUser, context=self.get_serializer_context()).data,
+            }
+        )
